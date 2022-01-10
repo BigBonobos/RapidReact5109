5f31acaf0a082b4a913896a9a8d1bec8f03c63bf
vidur2 <vmod2005@gmail.com>
2022-01-10 09:47:21 -0500

    youtube-dl of videos more fleshed pout

--- image_processing_code/image_getter.py
+++ image_processing_code/image_getter.py
@@ -6,13 +6,49 @@ Vidur Modgil
 '''
 
 # Imports
-import requests
-import youtube_dl
+from youtube_dl import YoutubeDL
+import cv2
+import os
 
 
 def main() :
-    ytdl = youtube_dl.YoutubeDL()
-    ytdl.download(["https://www.youtube.com/watch?v=OXkeAkWwex8"])
+    videos = []
+    ytdl = YoutubeDL()
+    
+    for i in range(3):
+        videos.append(f"https://www.youtube.com/watch?v=OXkeAkWwex8&list=PLZT9pIgNOV6a_P3_HDTolvzprSdYB_zd3&index={i + 1}")
+    ytdl.download(videos)
+
+    for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
+
+        for file in filenames:
+
+            extension = file.split(".")[1]
+
+            if (extension == "mp4"):
+
+                cam = cv2.VideoCapture(file)
+                if not os.path.exists("./image_data"):
+                    os.makedirs("./image_data")
+
+                ret = True
+                current_frame = 0
+
+                while (ret):
+                    ret, frame = cam.read()
+
+                    name = f"./image_data/frame {current_frame}.jpg"
+                    print(f"Creating {name}")
+
+                    cv2.imwrite(name, frame)
+
+                    current_frame += 1
+
+                cam.release()
+                cv2.destroyAllWindows()
+
+
+
 
 
 if __name__ == '__main__':

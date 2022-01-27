419339e4a9021d8a20ae0821e8776769dd6e086b
vidur2 <vmod2005@gmail.com>
2022-01-27 17:44:03 -0500

    bullshit

---
+++ Robot Code/nano_psuedocode/raspi_psuedo_code.py
@@ -0,0 +1,11 @@
+
+from networktables import NetworkTables
+
+
+def main():
+    nt_conn = NetworkTables.initialize(server='roborio-5109-frc.local')
+    testTable = NetworkTables.getTable("tester")
+    testTable.putNumber("shit", 5)
+
+if (__name__ == "__main__"):
+    main()

336fb3722d26ce9d5aeac8067354fa5eeb2d0f5b
BigBonobos <44553743+BigBonobos@users.noreply.github.com>
2022-03-03 01:51:35 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,21 +4,53 @@
 
 package frc.robot;
 
+import com.revrobotics.*;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.controller.BangBangController;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-
+  
+  //Controllers
+  public XboxController xBox = new XboxController(1);
   public Joystick j_Operator = new Joystick(1);
+
+  //Sensors
+  public DigitalInput Beam1 = new DigitalInput(0); //Outside the kicker wheel
+  public DigitalInput Beam2 = new DigitalInput(1); //Inside the kicker wheel
+  
+  //Variables
+  private int BallCount; 
+  private boolean shooting;
+  public boolean intakeOn = false; 
+  public boolean intakeExtend;
+  public boolean boolBeam2; 
+
+  //Motor
+  private CANSparkMax m_indexWheel = new CANSparkMax(23, MotorType.kBrushless);
+  private CANSparkMax m_shooterWheel = new CANSparkMax(24, MotorType.kBrushless);
+  private CANSparkMax m_intakeWheel = new CANSparkMax(25, MotorType.kBrushless); 
+
+  //Solenoids
+  private Solenoid s_LeftIntake = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
+  private Solenoid s_RightIntake = new Solenoid(PneumaticsModuleType.CTREPCM, 2);
+
+  //Encoders
+  private RelativeEncoder e_shooterWheel = m_shooterWheel.getEncoder();
+  private RelativeEncoder e_indexWheel = m_indexWheel.getEncoder();
+  private SparkMaxPIDController p_indexWheel = m_indexWheel.getPIDController();
+
+  //Controllers
+  private BangBangController overSpeedController; 
+  private int shooterRPMs = 1000; 
   
   @Override
   public void robotInit() {
   }
-
-  @Override
-  public void robotPeriodic() {}
 
   @Override
   public void autonomousInit() {
@@ -32,7 +64,24 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    SmartDashboard.putNumber("BallConut", BallCount);
+    SmartDashboard.putBoolean("IntakeMotor", intakeOn);
+    SmartDashboard.putBoolean("Shooting", shooting); 
+    Index();
+
+    /*if (xBox.getAButton()){
+      intakeSolenoid(intakeExtend = !intakeExtend);
+    }*/
+
+    if (xBox.getLeftBumper()){
+      intakeMotor(intakeOn = !intakeOn);
+    }
+
+    if (xBox.getRightBumper()){
+      Shooting();
+    }
+  }
 
   @Override
   public void disabledInit() {}
@@ -41,9 +90,104 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {}
 
   @Override
-  public void testInit() {}
+  public void testInit() {
+    e_indexWheel.setPosition(0);
+  }
 
-  /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    
+    SmartDashboard.putBoolean("BeamBreak", boolBeam2);
+    SmartDashboard.putNumber("IndexEncoders", e_indexWheel.getPosition());
+    //Orientation of Beam Breaks
+    if (Beam2.get()){
+      boolBeam2 = false;
+    }
+    else{
+      boolBeam2 = true; 
+    }
+
+    //Orientation of Shooter
+    m_shooterWheel.set(0.3);
+    //Orientation of Intake
+    m_intakeWheel.set(0.3);
+    //Orientation of index Motor
+    m_indexWheel.set(0.3);
+
+    //Determing how many encoders = 1 ball shot
+    /*if (xBox.getLeftBumper()){
+      m_indexWheel.set(0.2);
+    }*/
+
+  }
+
+
+  public void Index() {
+    if (shooting == false){
+        if (BallCount == 0){
+            if (!Beam1.get()){
+                m_indexWheel.set(0.4);
+            }
+            if (!Beam2.get()){
+                m_indexWheel.stopMotor();
+                BallCount = 1; 
+            }
+        }
+        if (BallCount == 1){
+            if (!Beam1.get() && !Beam2.get()){
+                m_indexWheel.stopMotor();
+                BallCount = 2; 
+            }
+            if (!Beam1.get() && Beam2.get()){
+                m_indexWheel.set(0.4);
+            }
+        }
+    }
+  }
+
+  public void Shooting() { 
+    shooting = true; 
+    if (BallCount > 0){
+        e_indexWheel.setPosition(0);
+        m_shooterWheel.set(overSpeedController.calculate(e_shooterWheel.getVelocity(), shooterRPMs));
+        if (e_shooterWheel.getVelocity() == shooterRPMs && e_indexWheel.getPosition() <= 1){
+            m_indexWheel.set(0.4);
+            BallCount --;
+        }
+    }
+    else{
+        m_shooterWheel.stopMotor();
+        shooting = false;
+    }
+   
+  }
+
+  /*public boolean intakeSolenoid(boolean intakeExtend){
+    if (intakeExtend == false){
+        s_LeftIntake.set(true);
+        s_RightIntake.set(true);
+        intakeExtend = true; 
+    }
+    if (intakeExtend == true){
+        s_LeftIntake.set(false);
+        s_RightIntake.set(false);
+        intakeExtend = false; 
+    }
+    return intakeExtend;
+  } */
+
+  public boolean intakeMotor(boolean intakeOn){
+    //if (intakeExtend == true){
+        if(intakeOn == false){
+            m_intakeWheel.set(0.3);
+            intakeOn = true; 
+        }
+        else{
+            m_intakeWheel.stopMotor();
+            intakeOn = false; 
+        }
+    //}
+    return intakeOn;
+  }
 }
+

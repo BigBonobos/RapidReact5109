c27e362218d38176aa5408defa9f4ca3f3fa8ba4
vidur2 <vmod2005@gmail.com>
2022-03-20 16:12:43 -0400

    more accurate jerk calculations

--- Robot Code/src/main/java/frc/robot/swerveCode/util/AccelContainer.java
+++ Robot Code/src/main/java/frc/robot/swerveCode/util/AccelContainer.java
@@ -11,6 +11,7 @@ public class AccelContainer {
     public Vector<AccelCoord> coordStorage = new Vector<>();
     public Optional<Notifier> accelCoordNotif = Optional.empty();
     public AHRS navX;
+    private Translation2d currentVelocity = new Translation2d(0, 0);
     Translation2d absolutePosition = new Translation2d(0, 0);
 
     public AccelContainer(AHRS superNav) {
@@ -20,14 +21,18 @@ public class AccelContainer {
     public void appendAccelCoord(Translation2d desiredVelocity) {
         if (accelCoordNotif.isPresent()) {
             accelCoordNotif.get().stop();
+            currentVelocity = coordStorage.get(coordStorage.size() - 1).getEndingVelocity();
         }
-        AccelCoord accelCoord = new AccelCoord(desiredVelocity, navX);
+        AccelCoord accelCoord = new AccelCoord(currentVelocity, desiredVelocity, navX);
         Notifier accelCoordNotifUw = new Notifier(accelCoord);
         accelCoordNotifUw.startPeriodic(0.001);
         accelCoordNotif = Optional.of(accelCoordNotifUw);
         coordStorage.add(accelCoord);
     }
-
+    public void resetOdometery() {
+        currentVelocity = new Translation2d(0, 0);
+        absolutePosition = new Translation2d(0, 0);
+    }
     public Translation2d getAbsolutePosTranslation2d() {
         if (accelCoordNotif.isPresent()) {
             accelCoordNotif.get().stop();
--- Robot Code/src/main/java/frc/robot/swerveCode/util/AccelCoord.java
+++ Robot Code/src/main/java/frc/robot/swerveCode/util/AccelCoord.java
@@ -10,12 +10,16 @@ public class AccelCoord implements Runnable {
     private HashMap<Double, Translation2d> accelMap = new HashMap<Double, Translation2d>();
     private double velocityX;
     private double velocityY;
+    private double vNotX;
+    private double vNotY;
     private double initTime;
     private AHRS navX;
 
-    public AccelCoord(Translation2d desiredVelocity, AHRS superNav) {
+    public AccelCoord(Translation2d initialVelocity, Translation2d desiredVelocity, AHRS superNav) {
         velocityX = desiredVelocity.getX();
         velocityY = desiredVelocity.getY();
+        vNotX = initialVelocity.getX();
+        vNotY = initialVelocity.getY();
         navX = superNav;
         initTime = superNav.getLastSensorTimestamp();
     }
@@ -29,11 +33,10 @@ public class AccelCoord implements Runnable {
         Double[] timeArray = accelMap.keySet().toArray(Double[]::new);
         Optional<Double> initTime = Optional.empty();
         Optional<Double> finalTime = Optional.empty();
-        double prevTime = 0;
         double displacementDueToAccelX = 0;
         double displacementDueToAccelY = 0;
 
-        for (int i = 0; i < timeArray.length; i++) {
+        for (int i = 0; i < timeArray.length - 1; i++) {
             if (Math.abs(accelMap.get(timeArray[i]).getNorm()) >= 0.01) {
                 if (initTime.isEmpty()) {
                     initTime = Optional.of(timeArray[i]);
@@ -41,24 +44,46 @@ public class AccelCoord implements Runnable {
                     finalTime = Optional.of(timeArray[i]);
                 }
             } else {
+                double prevTime;
+                if (i == 0) {
+                    prevTime = 0;
+                } else {
+                    prevTime = timeArray[i-1];
+                }
                 double currentTime = timeArray[i];
-                double accelX = accelMap.get(currentTime).getX();
-                double accelY = accelMap.get(currentTime).getY();
+
+                // Translates navX accel to terms of m_swerve accel (reflect accros y = -x)
+                double accelXCurr = -accelMap.get(currentTime).getY();
+                double accelYCurr = -accelMap.get(currentTime).getX();
+                double accelXPrev = -accelMap.get(prevTime).getY();
+                double accelYPrev = -accelMap.get(prevTime).getX();
                 double delta = currentTime - prevTime;
-                displacementDueToAccelX += accelX * delta * delta;
-                displacementDueToAccelY += accelY * delta * delta;
-                prevTime = currentTime;
+                double jerkX = (accelXCurr + accelXPrev)/2;
+                double jerkY = (accelYCurr + accelYPrev)/2;
+
+                // Calculates displacement
+                displacementDueToAccelX += (jerkX * delta * delta * delta)/6 + (accelXPrev * delta * delta)/2 + vNotX * delta;
+                displacementDueToAccelY +=  (jerkY * delta * delta * delta)/6 + (accelYPrev * delta * delta)/2 + vNotX * delta;
+
+                // Calculates velocity
+                vNotX += (jerkX * delta * delta)/2 + accelXPrev * delta;
+                vNotY += (jerkY * delta * delta)/2 + accelYPrev * delta;
             }
         }
         if (finalTime.isPresent() && initTime.isPresent()) {
             double displacementDueToVelocityX = (finalTime.get() - initTime.get()) * velocityX;
             double displacementDueToVelocityY = (finalTime.get() - initTime.get()) * velocityY;
-            return new Translation2d(displacementDueToVelocityX - displacementDueToAccelY, displacementDueToVelocityY - displacementDueToAccelX);
+            Translation2d returnValues = new Translation2d(displacementDueToVelocityX + displacementDueToAccelX, displacementDueToVelocityY + displacementDueToAccelY);
+            return returnValues;
         } else {
-            return new Translation2d(displacementDueToAccelX, displacementDueToAccelY);
+            Translation2d returnValues=  new Translation2d(displacementDueToAccelX, displacementDueToAccelY);
+            return returnValues;
         }
     }
 
+    public Translation2d getEndingVelocity() {
+        return new Translation2d(vNotX, vNotY);
+    }
     @Override
     public void run() {
         updateAccel();
